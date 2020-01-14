f9fc59916ba952148da368d2e2804274e587c3a3
강원우 <kangww@brandi.co.kr>
2020-01-14 19:31:56 +0900

    리스트 테스트

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <sui-comment>{{message}}</sui-comment>
+        <sui-segment v-for="color in colorList" :color="color" inverted>{{color}}</sui-segment>
     </div>
 </template>
 
@@ -12,7 +13,12 @@
         components: {},
         data: () => {
             return {
-                message: 'Have a Beautiful Day!!'
+                message: 'Have a Beautiful Day!!',
+                colorList: [
+                    'red', 'orange', 'yellow', 'olive', 'green',
+                    'teal', 'blue', 'violet', 'purple', 'pink',
+                    'brown', 'grey', 'black'
+                ]
             }
         },
         created: function(){
